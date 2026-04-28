1b6ad09a734be324ffd9908dd681a90811153c1d
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-04-27 21:28:54 -0700

    feat: add reviewId validation to trigger notFound for invalid reviews

--- rounting-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ rounting-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default async function ReviewProduct({
   params,
 } : {
   params : Promise<{productId: string, reviewId: string}>
 }){
   const { productId, reviewId } = await params;
+  if(parseInt(reviewId) > 1001){
+    notFound();
+  }
   return(
     <h1>Review {reviewId} for Product {productId}</h1>
   )
